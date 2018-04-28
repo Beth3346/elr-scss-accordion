e2628a84df8ac62e621c49bb7ecbc349ca734250
Elizabeth Rogers <beth3346@gmail.com>
2018-04-28 15:09:17 -0500

    update gulp file; remove babel

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var del = require('del');
+var path = require('path');
+var sass = require('gulp-sass');
+var scsslint = require('gulp-scss-lint');
+var plumber = require('gulp-plumber');
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+
+var paths = {
+  app: 'dist/',
+  css: 'dist/css/'
+};
+
+gulp.task('clean:styles', function() {
+  return del([paths.css]);
+});
+
+gulp.task('scsslint', function() {
+  return gulp.src('src/**/*.scss').pipe(scsslint());
+});
+
+gulp.task('styles', ['scsslint'], function() {
+  return gulp
+    .src('style.scss')
+    .pipe(plumber())
+    .pipe(
+      sass({
+        includePaths: ['node_modules']
+      }).on('error', sass.logError)
+    )
+    .pipe(gulp.dest(paths.css))
+    .pipe(
+      autoprefixer({
+        browsers: ['last 3 versions'],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest(paths.css))
+    .pipe(cleanCSS({ debug: true }))
+    .pipe(gulp.dest(paths.css));
+});
+
+gulp.task('default', ['styles']);
